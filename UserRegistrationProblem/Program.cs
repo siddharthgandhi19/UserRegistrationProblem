d1257a43b5c0fed52cf10dbefb8b9b746443b4b9
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-29 14:35:13 +0530

    [Siddharth]Add:Enter FirstName starts with Cap and has minimum 3 characters

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace UserRegistrationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problem Statements");
+            Console.WriteLine("Select Below Mention Options");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine(" 1. First Name");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter First Name");
+                        string opt = Convert.ToString(Console.ReadLine());
+                        UserRegistration firstname = new UserRegistration();
+                        firstname.FirstName(opt);
+                        break;
+                    default:
+                        flag = false;
+                        Console.WriteLine("Try Again");
+                        break;
+                }
+
+            }
+
+        }
+    }
+}
---
+++ UserRegistrationProblem/UserRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public  class UserRegistration
+    {
+        const string FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public void FirstName(string input)
+        {
+            if (Regex.IsMatch(input, FIRST_NAME))
+                Console.WriteLine("First Name : " + input);
+            else
+                Console.WriteLine("Enter First Name In Valid Format");
+        }
+    }
+}
